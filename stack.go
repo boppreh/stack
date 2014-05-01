966c2feeff3f0b5d9aef126352caad81af90b672
BoppreH <boppreh@gmail.com>
2014-05-01 10:34:38 -0300

    Add stack structure

--- stack.go
+++ stack.go
@@ -2,10 +2,24 @@ package stack
 
 import "fmt"
 
-func Text() string {
-	return "Hello Worlds"
+type Value int
+
+type node struct {
+	value Value	
+	next *node
 }
 
-func Say() {
-	fmt.Println(Text())
+type Stack struct {
+	top *node
+}
+
+func (s *Stack) push(vs ...Value) {
+	for _, value := range vs {
+		s.top = &node{value, s.top}
+	}
+}
+
+func (s *Stack) pop() (v Value) {
+	v, s.top = s.top.value, s.top.next
+	return
 }
--- stack_test.go
+++ stack_test.go
@@ -2,10 +2,29 @@ package stack
 
 import "testing"
 
-func TestText(t *testing.T) {
-	result := Text()
-	expected := "Hello Worlds"
+func assert(t *testing.T, result Value, expected Value) {
 	if result != expected {
-		t.Errorf("Expected %q got %q", expected, result)
+		t.Errorf("Expected %v, got %v", expected, result)
 	}
+}
+
+func assertStack(t *testing.T, stack *Stack, expectedValues ...Value) {
+	for key, value := range expectedValues {
+		popped := stack.pop()
+		if popped != value {
+			t.Errorf("Expected value %v to be %v, got %v instead.", key, value, popped)
+		}
+	}
+}
+
+func TestStackStruct(t *testing.T) {
+	s := new(Stack)
+
+	s.push(10)
+	assert(t, s.pop(), 10)
+
+	s.push(20)
+	s.push(15)
+	s.push(1)
+	assertStack(t, s, 1, 15, 20)
 }
