dca06c338a8a38b255baf4c8405523c199969f5d
BoppreH <boppreh@gmail.com>
2014-05-01 23:57:21 -0300

    Move operators to different file

--- lib/vm.go
+++ lib/vm.go
@@ -2,72 +2,12 @@ package lib
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"unicode"
 )
 
 type In func() Value
 type Out func(Value)
-
-func sAdd(i In, o Out) { o(i().(int) + i().(int)) }
-func sSub(i In, o Out) { o(i().(int) - i().(int)) }
-func sDiv(i In, o Out) { o(i().(int) / i().(int)) }
-func sMul(i In, o Out) { o(i().(int) * i().(int)) }
-
-func sMap(i In, o Out) {
-	fnList := i().([]Value)
-	list := i().([]Value)
-
-	fullFnList := make([]Value, len(fnList)+1)
-	copy(fullFnList[1:], fnList)
-
-	for i := range list {
-		fullFnList[0] = list[i]
-		result, _ := Run(fullFnList)
-		list[i] = result[0]
-	}
-
-	o(list)
-}
-
-func sRun(i In, o Out) {
-	code := i().([]Value)
-	result, _ := Run(code)
-	for _, value := range result {
-		o(value)
-	}
-}
-
-func sIf(i In, o Out) {
-	value := i()
-	else_ := i().([]Value)
-	then := i().([]Value)
-
-	var condition bool
-	switch value.(type) {
-	case int:
-		condition = value != 0
-	case string:
-		condition = value != ""
-	default:
-		fmt.Println("Ops:", value)
-	}
-
-	if condition {
-		o(then)
-	} else {
-		o(else_)
-	}
-
-	sRun(i, o)
-}
-
-func sPrint(i In, o Out) {
-	v := i()
-	fmt.Println(v)
-	o(v)
-}
 
 func ignoreComment(input chan rune) {
 	for {
