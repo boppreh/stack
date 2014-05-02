8a05da53ef20653d96e9d45fa5739fd6ec4f2de4
BoppreH <boppreh@gmail.com>
2014-05-02 02:09:20 -0300

    Simplify tests signature

--- core/vm_test.go
+++ core/vm_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func assertParse(t *testing.T, sourceCode string, expected []Value) {
+func assertParse(t *testing.T, sourceCode string, expected... Value) {
 	result, err := Parse(sourceCode)
 	if err != nil {
 		t.Error(err)
@@ -12,7 +12,7 @@ func assertParse(t *testing.T, sourceCode string, expected []Value) {
 	assert(t, result, expected)
 }
 
-func assertRun(t *testing.T, sourceCode string, expected []Value) {
+func assertRun(t *testing.T, sourceCode string, expected... Value) {
 	program, err := Parse(sourceCode)
 	if err != nil {
 		t.Error(err)
@@ -25,71 +25,69 @@ func assertRun(t *testing.T, sourceCode string, expected []Value) {
 }
 
 func TestParse(t *testing.T) {
-	assertParse(t, "", []Value{})
-	assertParse(t, "   ", []Value{})
-	assertParse(t, " \n  ", []Value{})
+	assertParse(t, "\"first string\"", "first string")
+	assertParse(t, ":symbol", "symbol")
+	assertParse(t, ":symbol :symbol2", "symbol", "symbol2")
+	assertParse(t, "\"a\" 'b' :symbol", "a", "b", "symbol")
 
-	assertParse(t, "\"first string\"", []Value{"first string"})
-	assertParse(t, ":symbol", []Value{"symbol"})
-	assertParse(t, ":symbol :symbol2", []Value{"symbol", "symbol2"})
-	assertParse(t, "\"a\" 'b' :symbol", []Value{"a", "b", "symbol"})
+	assertParse(t, "1", 1)
+	assertParse(t, "12345", 12345)
+	assertParse(t, "123 321", 123, 321)
 
-	assertParse(t, "1", []Value{1})
-	assertParse(t, "12345", []Value{12345})
-	assertParse(t, "123 321", []Value{123, 321})
+	assertParse(t, "#Comment\n123", 123)
+	assertParse(t, "123\n321 # Comment\n123", 123, 321, 123)
+	assertParse(t, "123\n321 # Comment\n123", 123, 321, 123)
 
-	assertParse(t, "#Comment", []Value{})
-	assertParse(t, "\n  #Comment", []Value{})
-	assertParse(t, "#Comment\n123", []Value{123})
-	assertParse(t, "123\n321 # Comment\n123", []Value{123, 321, 123})
-	assertParse(t, "123\n321 # Comment\n123", []Value{123, 321, 123})
-
-	assertParse(t, "[]", []Value{[]Value{}})
-	assertParse(t, "[1 2 3]", []Value{[]Value{1, 2, 3}})
-	assertParse(t, "[1 2 3] [] [1]", []Value{[]Value{1, 2, 3}, []Value{}, []Value{1}})
-	assertParse(t, "[[]]", []Value{[]Value{[]Value{}}})
-	assertParse(t, "[1 [2 3] 4]", []Value{[]Value{1, []Value{2, 3}, 4}})
-	assertParse(t, "[[2 3]]", []Value{[]Value{[]Value{2, 3}}})
+	assertParse(t, "[]", []Value{})
+	assertParse(t, "[1 2 3]", []Value{1, 2, 3})
+	assertParse(t, "[1 2 3] [] [1]", []Value{1, 2, 3}, []Value{}, []Value{1})
+	assertParse(t, "[[]]", []Value{[]Value{}})
+	assertParse(t, "[1 [2 3] 4]", []Value{1, []Value{2, 3}, 4})
+	assertParse(t, "[[2 3]]", []Value{[]Value{2, 3}})
 }
 
 func TestRun(t *testing.T) {
-	assertRun(t, "1", []Value{1})
-	assertRun(t, "1 2 3", []Value{3, 2, 1})
-	assertRun(t, "1 1 +", []Value{2})
-	assertRun(t, "1 1 + 2 *", []Value{4})
+	assertRun(t, "1", 1)
+	assertRun(t, "1 2 3", 3, 2, 1)
+	assertRun(t, "1 1 +", 2)
+	assertRun(t, "1 1 + 2 *", 4)
 
-	assertRun(t, "1 1 =", []Value{true})
-	assertRun(t, "1 2 <", []Value{true})
-	assertRun(t, "1 2 >", []Value{false})
+	assertRun(t, "1 1 =", true)
+	assertRun(t, "1 2 <", true)
+	assertRun(t, "1 2 >", false)
 
-	assertRun(t, "[1 2 3] [1 +] %", []Value{[]Value{2, 3, 4}})
-	assertRun(t, "[1 2 3] [1 +] % [2 *] %", []Value{[]Value{4, 6, 8}})
-	assertRun(t, "[1 2 3] [1 + 2 *] %", []Value{[]Value{4, 6, 8}})
+	assertRun(t, "[1 2 3] [1 +] %", []Value{2, 3, 4})
+	assertRun(t, "[1 2 3] [1 +] % [2 *] %", []Value{4, 6, 8})
+	assertRun(t, "[1 2 3] [1 + 2 *] %", []Value{4, 6, 8})
 
-	assertRun(t, "[10] !", []Value{10})
-	assertRun(t, "[10 20] !", []Value{10, 20})
-	assertRun(t, "[1 1 +] !", []Value{2})
+	assertRun(t, "[10] !", 10)
+	assertRun(t, "[10 20] !", 10, 20)
+	assertRun(t, "[1 1 +] !", 2)
 
-	assertRun(t, "1 [1] [2] ?", []Value{1})
-	assertRun(t, "0 [1] [2] ?", []Value{2})
-	assertRun(t, "1 [1 1 +] [2 2 +] ?", []Value{2})
-	assertRun(t, "0 [1 1 +] [2 2 +] ?", []Value{4})
-	assertRun(t, "0 0 = [1] [2] ?", []Value{1})
-	assertRun(t, "0 0 = [1] [2] ?", []Value{1})
+	assertRun(t, "1 [1] [2] ?", 1)
+	assertRun(t, "0 [1] [2] ?", 2)
+	assertRun(t, "1 [1 1 +] [2 2 +] ?", 2)
+	assertRun(t, "0 [1 1 +] [2 2 +] ?", 4)
+	assertRun(t, "0 0 = [1] [2] ?", 1)
+	assertRun(t, "0 0 = [1] [2] ?", 1)
 
-	assertRun(t, "1 [2] :a $", []Value{1})
-	assertRun(t, "[1 1 +] :a $ :a @", []Value{2})
+	assertRun(t, "1 [2] :a $", 1)
+	assertRun(t, "[1 1 +] :a $ :a @", 2)
 
-	assertRun(t, "1 .", []Value{1, 1})
-	assertRun(t, "1 2 . + +", []Value{5})
+	assertRun(t, "1 .", 1, 1)
+	assertRun(t, "1 2 . + +", 5)
 
-	assertRun(t, "[. 5 < [1 + :inc @] [] ?] :inc $   1 :inc @", []Value{5})
+	assertRun(t, "[. 5 < [1 + :inc @] [] ?] :inc $   1 :inc @", 5)
 
-	assertRun(t, "[1 2 3] 4 append", []Value{[]Value{1, 2, 3, 4}})
+	assertRun(t, "[1 2 3] 4 append", []Value{1, 2, 3, 4})
 
-	assertRun(t, "[1 1 +] eval", []Value{2})
-	assertRun(t, "5 dup", []Value{5, 5})
+	assertRun(t, "[1 1 +] eval", 2)
+	assertRun(t, "5 dup", 5, 5)
 
-	assertRun(t, ":5 int", []Value{5})
-	assertRun(t, "5 str", []Value{"5"})
+	assertRun(t, ":5 int", 5)
+	assertRun(t, "5 str", "5")
+}
+
+func TestLib(t *testing.T) {
+	//assertRun(t, "", []Value)
 }
