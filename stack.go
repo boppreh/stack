570b601b6bca0d70dc08518d56cd503fa43b102d
BoppreH <boppreh@gmail.com>
2014-05-01 23:45:22 -0300

    Add REPL

--- stack.go
+++ stack.go
@@ -4,24 +4,12 @@ import (
 	"github.com/boppreh/stack/lib"
 	"fmt"
 	"os"
+	"bufio"
 	"io/ioutil"
 )
 
-func main() {
-	var bytes []byte
-	var err error
-
-	if len(os.Args) == 1 {
-		bytes, err = ioutil.ReadAll(os.Stdin)	
-	} else {
-		bytes, err = ioutil.ReadFile(os.Args[1])
-	}
-
-	if err != nil {
-		panic(err)
-	}
-
-	program, err := lib.Parse(string(bytes))
+func runAndPrint(sourceCode string) {
+	program, err := lib.Parse(sourceCode)
 	result, err := lib.Run(program)
 	if err != nil {
 		panic(err)
@@ -29,5 +17,25 @@ func main() {
 
 	for _, value := range result {
 		fmt.Println(value)
+	}
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		for {
+			fmt.Print(">> ")
+			bio := bufio.NewReader(os.Stdin)
+			line, _, err := bio.ReadLine()
+			if err != nil {
+				panic(err)
+			}
+			runAndPrint(string(line))
+		}
+	} else {
+		bytes, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		runAndPrint(string(bytes))
 	}
 }
